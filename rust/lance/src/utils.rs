a7632d9258ba846eeb3277414643b589e8e3a816
Xuanwo <github@xuanwo.io>
2025-11-19 18:45:49 +0800

    Revert utils
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- rust/lance/src/utils.rs
+++ rust/lance/src/utils.rs
@@ -7,7 +7,5 @@ pub(crate) mod future;
 pub(crate) mod temporal;
 #[cfg(test)]
 pub(crate) mod test;
-
-// Re-export
-pub use lance_datafusion::sql;
-pub use lance_index::vector::kmeans;
+#[cfg(feature = "tensorflow")]
+pub mod tfrecord;
